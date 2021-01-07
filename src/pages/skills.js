0ceb98b91abdf5832fa8cb326af72233cef0258e
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-01-07 13:03:06 -0700

    added icons to skill cards

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -3,9 +3,14 @@ import { Link } from "gatsby"
 import {skills} from '../data/skills'
 import { BiWindowAlt, BiData } from 'react-icons/bi'
 import { GiSkills } from 'react-icons/gi'
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const icons = [
+  BiData,
+  BiWindowAlt,
+  GiSkills
+]
 
 const links = ['Home', 'Projects', 'About']
 const linkList = links.map(link => {
@@ -15,10 +20,15 @@ const linkList = links.map(link => {
 })
 
 const Skills = () => {
-  const skillDeck = skills.map(skill => {
+  const skillDeck = skills.map((skill, index) => {
+    const Icon = icons[index];
+    console.log(Icon);
+    
     return(
       <div className="skillCard">
-        {`<${skill.icon} />`}
+        <div className="skillCardIconContainer">
+          <Icon className="skillCardIcon"/>
+        </div>
         <h3>{skill.title}</h3>
         <hr className="skillHr"/>
         {skill.skills.map(skillName => {
